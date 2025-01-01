56268a991fde51f4c54781e1ccb4048f1422a266
zulkarnen <zulkarnen1900016072@webmail.uad.ac.id>
2025-01-01 14:38:03 +0700

    feat(fetch-app-go): initial

--- auth-node/.docker/entrypoint.sh
+++ auth-node/.docker/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sleep 10
+sleep 5
 
 npx prisma migrate dev
 
--- auth-node/src/versions/v1/index.ts
+++ auth-node/src/versions/v1/index.ts
@@ -1,9 +1,7 @@
 import { Router } from "express";
-import users from "./user";
 import auth from "./auth";
 const v1 = Router();
 
-v1.use("/users", users);
 v1.use("/auth", auth);
 
 export default v1;
